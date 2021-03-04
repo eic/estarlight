3c105a5a9609eba389a214d407c83fe299f54eec
Samuel Heppelmann <sheppelmann@ucdavis.edu>
2021-03-03 18:33:01 -0800

    Added comments

--- src/gammaavm.cpp
+++ src/gammaavm.cpp
@@ -628,37 +628,42 @@ void Gammaavectormeson::momenta(double W,double Egam,double Q2, double gamma_pz,
 	Y = 0.5*std::log( (E+fabs(pz))/(E-fabs(pz)) );
 	//	cout << "pz_final = " << pz << endl;
 
-	
+
+	// Backwards Production
 	if (_backwardsProduction) {
-	  // use backward production
-	  double pz_tar_i_lab = -1.0*target_pz;
-	  double E_tar_i_lab = sqrt(pow(target_pz,2) + pow(starlightConstants::protonMass,2.));
-	  double pz_gam_i_lab = Egam;
-	  double E_gam_i_lab = Egam;
-	  
+	  //Fill the intial state in the lab frame
+	  double pz_tar_i_lab = -1.0*target_pz;  //Flip the pz of the target
+	  double E_tar_i_lab = sqrt(pow(target_pz,2) + pow(starlightConstants::protonMass,2.)); //Calculate proton energy
+	  double pz_gam_i_lab = Egam; //Photon energy
+	  double E_gam_i_lab = Egam;  //Photon energy
+
+	  //Calculate the final state target energy and pz
 	  double pz_tar_f_lab = (pz_tar_i_lab + pz_gam_i_lab) - pz;
 	  double E_tar_f_lab = sqrt( pow(pz_tar_f_lab,2) + pow(starlightConstants::protonMass,2.) );
-	  
+
+	  //Calculate the boost (beta,gamma) to the center of mass frame
 	  double beta = (pz_tar_i_lab + pz_gam_i_lab)/(E_gam_i_lab + E_tar_i_lab);
 	  double lorentzGamma = 1.0/sqrt( 1 - pow(beta,2.));
 	  
-	  //Boost to CM frame
+	  //Boost to the final state target to the CM frame
 	  double cm_frame_target_pz = lorentzGamma*( pz_tar_f_lab + beta*E_tar_f_lab );
 	  double cm_frame_gamma_pz  = lorentzGamma*( pz + beta*pz );
 	  
-	  //Recalculate Energy
+	  //Recalculate target energy in the CM frame (assign to VM) 
 	  double vm_mass = sqrt( E*E - ( px*px + py*py + pz*pz ) );
 	  double vm_E_cm = sqrt( vm_mass*vm_mass + px*px + py*py + cm_frame_target_pz*cm_frame_target_pz);
 
+	  //Calcuate photon energy in CM frame (assign to target)
 	  double cm_E = sqrt( pow(starlightConstants::protonMass,2.)  + pow(t_px,2.) + pow(t_py,2.) + pow(cm_frame_gamma_pz,2.));	  
-	  	  
-	  //Boost back to lab frame
+	  //Boost photon and target back to lab frame
+	  //target:
 	  t_pz = lorentzGamma*(cm_frame_gamma_pz - beta*cm_E);
 	  t_E = lorentzGamma*(cm_E - beta*cm_fame_gamma_pz);
-
+	  //Photon:
 	  pz = lorentzGamma*(cm_frame_target_pz - beta*vm_E_cm);
 	  E = lorentzGamma*(vm_E_cm - beta*cm_frame_target_pz);
-	  
+
+	  //Calculate the rapidity
 	  Y = 0.5*std::log( (E+fabs(pz))/(E-fabs(pz)) );
 	}
 	
