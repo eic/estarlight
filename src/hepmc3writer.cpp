c923d8a5e2978ea0bf2a86867782aa67c1379da9
Zachary Sweger <zacharysweger@Zacharys-MacBook-Pro.local>
2021-08-09 16:49:47 -0700

    Added event numbering to hepmc3 output

--- src/hepmc3writer.cpp
+++ src/hepmc3writer.cpp
@@ -33,6 +33,7 @@ int hepMC3Writer::writeEvent(const eXEvent &event, int eventnumber)
 
   /** Make HepMC3 Event and Vertex ... Currently only 1 Vertex per Event  **/ 
   HepMC3::GenEvent hepmc3_evt(HepMC3::Units::GEV , HepMC3::Units::MM);
+  hepmc3_evt.set_event_number( eventnumber );
   HepMC3::GenVertexPtr hepmc3_vertex_to_write = std::make_shared<HepMC3::GenVertex>(FourVector(0,0,0,0));
 
   /** Get starlight particle vector **/
